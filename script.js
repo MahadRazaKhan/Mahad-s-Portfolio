bf5f4f0913b14e3a55b5ca02823af0611bd49085
Mahad Raza Khan <130212940+MahadRazaKhan@users.noreply.github.com>
2024-08-27 11:46:15 +0500

    add script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Projects />
+      <About />
+      <Contact />
+      <footer>
+        <p>&copy; 2024 My Portfolio. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
